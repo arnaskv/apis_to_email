f42b03a944e069724401aa35a5cc0100c61a3ea1
Arnas Kvedaravičius <102704277+arnaskv@users.noreply.github.com>
2023-07-21 18:02:22 +0300

    Update test_apis_to_email.py

--- test_apis_to_email.py
+++ test_apis_to_email.py
@@ -1,8 +1,21 @@
-import apis_to_email
+from apis_to_email import check_email
+from apis_to_email import check_api
 import pytest
 
+
 def main():
-    pass
+    test_check_api()
+    test_check_api()
+
 
 def test_check_email():
-    pass
+    with pytest.raises(SystemExit):
+        check_email("edmund@gmailcom")
+        check_email("ed123gmail.com")
+        check_email("myemail")
+
+
+def test_check_api():
+    with pytest.raises(SystemExit):
+        check_api("guard")
+        check_api("lastfm")
